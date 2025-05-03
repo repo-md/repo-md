40e519d3589ebddcc9c7e5cec380fccb2f630b32
felix <you@example.com>
2025-05-03 10:57:27 -0500

    .

--- src/demo/vite.config.ts
+++ src/demo/vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/repo-client/', // Base path for GitHub Pages
+  base: '/repo-md/', // Base path for GitHub Pages
   build: {
     outDir: '../../demo'
   },
